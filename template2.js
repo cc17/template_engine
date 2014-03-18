4c3d513af7595628219f2e8e6e534a91385d3d67
wuya <dengxiaoming1217@gmail.com>
2014-03-18 19:04:13 +0800

    自己根据网上流传版本创建的简单版本

---
+++ template2.js
@@ -0,0 +1,38 @@
+function template(tpl,data){
+		var reg = /<%([^%>]+)%>/g,
+			regExp = /(^( )?(if|else|switch|case|break|{|}))(.*)?/g,
+			result,
+			cursor = 0,
+			temp = 'with(data){var arr = [];\n';
+
+		var add = function(code,js){
+			js ? temp += code.match(regExp) ? code : 'arr.push('+ code +');\n'
+				: temp += 'arr.push("'+ code.replace(/"/g,'\\"') +'");\n';
+		};
+		while(result = reg.exec(tpl)){
+			add(tpl.slice(cursor,result.index));
+			add(result[1],true);
+			cursor = result.index + result[0].length;
+		}
+		temp += 'return arr.join("");\n';
+		temp += '}';
+		//console.log(temp);
+		var fn = new Function('data',temp);
+		fn(data);
+		var returnStr = fn(data);
+		return returnStr;
+	};	
+	var tpl = 'my name is '
+		+ '<%if(name){%>'
+			+'<%name%>'
+		+ '<%}%>'
+		+ ',age is <%age%>.当哩个当 <%post.message%>';
+
+	var result = template(tpl,{
+		name:'dxm',
+		age:27,
+		post:{
+			message:'nha'
+		}
+	});
+	console.log(result);
